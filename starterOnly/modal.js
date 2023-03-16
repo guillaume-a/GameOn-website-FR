60553f3f0e3f3862e843b638b3209578689cc50b
Guillaume Aveline <guillaume@codr.fr>
2023-03-16 09:51:31 +0100

    Ref #1: fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,21 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const closeBtn = document.querySelector(".bground .close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// Close modal by clicking on background
+modalbg.addEventListener('click', () => modalbg.style.display = "none")
+
+// Close modal by clicking on close button
+closeBtn.addEventListener('click', () => modalbg.style.display = "none")
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 
